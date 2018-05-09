ca8fa6c78831c303a59e1c36a838dff776713a14
Wellington A. Silva <wellingtonasilva@gmail.com>
2018-05-09 12:59:51 -0300

    1. Adicionado configuração e exemplo de acesso a base de dados Postgres

--- src/main/java/br/com/wsilva/custom/bean/Car.java
+++ src/main/java/br/com/wsilva/custom/bean/Car.java
@@ -2,31 +2,60 @@ package br.com.wsilva.custom.bean;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import br.com.wsilva.custom.model.CarEntity;
 
 @Named("car")
 @RequestScoped
-public class Car 
-{
+public class Car {
 	private String name;
 	private Integer year;
 	private Float price;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Integer getYear() {
 		return year;
 	}
+
 	public void setYear(Integer year) {
 		this.year = year;
 	}
+
 	public Float getPrice() {
 		return price;
 	}
+
 	public void setPrice(Float price) {
 		this.price = price;
+	}
+
+	public String save() {
+		CarEntity entity = new CarEntity();
+		entity.setName(name);
+		entity.setPrice(price);
+		entity.setYear(year);
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("datasource");
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		em.persist(entity);
+		tx.commit();
+
+		em.close();
+		emf.close();
+
+		return "confirmation";
 	}
 }
---
+++ src/main/java/br/com/wsilva/custom/model/CarEntity.java
@@ -0,0 +1,60 @@
+package br.com.wsilva.custom.model;
+
+import java.math.BigInteger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="car")
+public class CarEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="car_id")
+	private BigInteger id;
+	
+	@Column(name="car_name")
+	private String name;
+	
+	@Column(name="car_year")
+	private Integer year;
+	
+	@Column(name="car_price")
+	private Float price;
+
+	public BigInteger getId() {
+		return id;
+	}
+
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+}
